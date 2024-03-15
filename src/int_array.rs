c58453156eb9f1fb124a8ac1368e54a95739a816
jimit <jimitsoni18@gmail.com>
2024-03-15 20:14:41 +0530

    fixed missing number implementation (raw)

--- src/int_array.rs
+++ src/int_array.rs
@@ -68,23 +68,62 @@ pub fn slice_union<T: Ord + Eq + Copy>(arr1: &[T], arr2: &[T]) -> Vec<T> {
 	return union;
 }
 
-// pub fn find_missing_number(arr: &[usize]) -> usize {
-// 	let proxy = arr.clone().sort();
+/// find a missing number from array with 1..N numbers.
+/// Works on the guarantee that exactly one number is missing
+/// and all the numbers are from 1 to N where N is length of slice
+pub fn find_missing_number(arr: &[usize]) -> usize {
+	let mut proxy = arr.to_vec();
+	proxy.sort();
 
-// 	fn binary_search_inner(arr: &[usize], start: usize, end: usize) -> usize {
-// 		let len = end - start;
+	if proxy[0] == 2 {
+		return 1;
+	}
 
-// 		todo!()
-// 	}
-// }
+	fn binary_search_inner(arr: Vec<usize>, start: usize, end: usize) -> usize {
+		if start > end {
+			panic!("wtf")
+		}
+		let len = end - start + 1;
+		let mid = start + ((len - (len % 2)) / 2);
+		if len == 2 {
+			if arr[mid - 1] + 2 == arr[mid] {
+				return arr[mid] - 1;
+			} else if arr[mid + 1] - 2 == arr[mid] {
+				panic!("wtfff!");
+			} else {
+				panic!("wtf");
+			}
+		}
+
+		if mid == arr[mid] - 1 {
+			return binary_search_inner(arr, mid, end);
+		} else if mid == arr[mid] - 2 {
+			return binary_search_inner(arr, start, mid);
+		} else {
+			let a = format!("wtf! mid: {mid}, arr[mid]: {}", arr[mid]);
+			panic!("{a}");
+		}
+
+		// todo!()
+	}
+
+	let len = proxy.len();
+
+	return binary_search_inner(proxy, 0, len - 1);
+
+	// todo!()
+}
 
 pub trait NumArrayInPlaceOperations {
 	fn move_zeros_to_end(&mut self);
 }
 
 impl NumArrayInPlaceOperations for [u32] {
-	// FIXME: THIS HAS THE WORST CASE TIME COMPLEXITY OF O(N^2)
+	// FIXME: OLD IMPLEMENTATION HAS THE WORST CASE TIME COMPLEXITY OF O(N^2)
 	// FIXME: USE OPTIMAL APPROACH
+
+	// FIXME: THE NEWER IMPLEMENTATION FAILS TESTS,
+	// FIXME: NEED TO REIMPLEMENT WITH FRESH MIND
 	fn move_zeros_to_end(&mut self) {
 		let len = self.len();
 		// OLD IMPLEMENTATION
@@ -102,19 +141,35 @@ impl NumArrayInPlaceOperations for [u32] {
 		let mut i = 0;
 		let mut j = 0;
 
-		while j < len {
-			while self[j] != 0 {
+		while j < len && i < len {
+			while j < len && self[j] != 0 {
 				j += 1;
 			}
 			if j == len - 1 {
-				if self[j] != 0 {
+				if self[j] != 0 && self[i] == 0 {
 					self.swap_in_place(i, j);
 				}
 				return;
 			}
 			i = j + 1;
+			if i >= len {
+				return;
+			}
 			if self[i] != 0 {
 				self.swap_in_place(i, j);
+			} else {
+				while i < len && self[i] == 0 {
+					i += 1;
+					// println!("=>> how many times here: {i}, self[i]: {}", self[i]);
+				}
+				println!("=>> what is i while exiting: {i}");
+				if i == len - 1 {
+					if self[i] != 0 {
+						self.swap_in_place(i, j);
+					}
+					println!("=>> i is len-1");
+					return;
+				}
 			}
 		}
 	}
@@ -127,11 +182,34 @@ mod tests {
 	fn test_moved_zeros_to_end() {
 		let mut arr = [2, 3, 5, 1, 2, 1, 1, 1, 1, 1, 4, 5];
 
-		println!("=>> before {arr:?}");
-
 		arr.move_zeros_to_end();
+		assert!(arr.eq(&[2, 3, 5, 1, 2, 1, 1, 1, 1, 1, 4, 5]));
+		println!("=>> test 1 complete");
 
-		println!("=>> after  {arr:?}");
+		let mut arr = [1, 3, 6, 0, 1, 0, 0, 0, 9];
+		arr.move_zeros_to_end();
+		assert!(arr.eq(&[1, 3, 6, 1, 9, 0, 0, 0, 0]));
+		println!("=>> test 2 complete");
+
+		let mut arr = [0, 3, 6, 0, 1, 0, 0, 9];
+		arr.move_zeros_to_end();
+		assert!(arr.eq(&[3, 6, 1, 9, 0, 0, 0, 0]));
+		println!("=>> test 3 complete");
+
+		let mut arr = [1, 3, 6, 0, 1, 0, 0, 0];
+		arr.move_zeros_to_end();
+		assert!(arr.eq(&[1, 3, 6, 1, 0, 0, 0, 0]));
+		println!("=>> test 4 complete");
+
+		let mut arr = [0, 3, 6, 1, 1, 1, 1, 0];
+		arr.move_zeros_to_end();
+		assert!(arr.eq(&[3, 6, 1, 1, 1, 1, 0, 0]));
+		println!("=>> test 5 complete");
+
+		let mut arr = [0, 0, 0, 0, 0, 0, 0, 0];
+		arr.move_zeros_to_end();
+		assert!(arr.eq(&[0, 0, 0, 0, 0, 0, 0, 0]));
+		println!("=>> test 6 complete");
 	}
 
 	#[test]
@@ -142,5 +220,32 @@ mod tests {
 		let union = slice_union(&arr1, &arr2);
 
 		assert!(union.eq(&[1, 2, 3, 4, 5, 6, 7, 8]))
+	}
+
+	#[test]
+	fn test_find_missing_number_found() {
+		let arr = [1, 2, 3, 4, 6, 7, 8];
+		let missing = find_missing_number(&arr);
+		assert_eq!(missing, 5);
+
+		let arr = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+		let missing = find_missing_number(&arr);
+		assert_eq!(missing, 1);
+
+		let arr = [1, 3, 4, 5, 6, 7, 8];
+		let missing = find_missing_number(&arr);
+		assert_eq!(missing, 2);
+
+		let arr = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+		let missing = find_missing_number(&arr);
+		assert_eq!(missing, 8);
+
+		let arr = [1, 2, 4, 5, 6, 7, 8, 9, 10];
+		let missing = find_missing_number(&arr);
+		assert_eq!(missing, 3);
+
+		let arr = [9, 2, 6, 8, 1, 3, 5, 7, 4, 11];
+		let missing = find_missing_number(&arr);
+		assert_eq!(missing, 10);
 	}
 }
